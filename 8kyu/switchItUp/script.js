16ac9a6461b164bc20483db81d50b91d76577d4f
Ian Stewart <ianstewart4@outlook.com>
2021-09-21 16:57:03 -0500

    adding switchItUp

---
+++ 8kyu/switchItUp/script.js
@@ -0,0 +1,29 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+// My solution
+
+function switchItUp(number) {
+    let numWord = {
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine",
+        0: "Zero"
+    }
+    return numWord[number]
+}
+
+// Favourite solution
+
+function switchItUp(n) {
+    return ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"][n]
+}
