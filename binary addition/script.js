ef38f8a2d35151ffc713fca04fbcbc7887291900
Ian Stewart <ianstewart4outlook.com>
2022-03-20 15:58:43 -0500

    adding binary adding kata

---
+++ binary addition/script.js
@@ -0,0 +1,25 @@
+// Details
+
+// Description:
+// Implement a function that adds two numbers together and returns their sum in binary. The conversion can be done before, or after the addition.
+
+// The binary number returned should be a string.
+
+// Examples:(Input1, Input2 --> Output (explanation)))
+
+// 1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+// 5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+
+
+
+// My Solution (4 mins)
+
+function addBinary(a,b){
+    return (a+b).toString(2) // Had to google this. when used on a number with 2 parameter, toString converts to binary, 
+}
+
+// Best Solution (Exact same as mine!)
+
+function addBinary(a,b){
+    return (a+b).toString(2)
+  }
