8b6a264bb010eaa5914ed89d198c7224f32d23eb
Ian Stewart <ianstewart4outlook.com>
2022-03-20 15:12:06 -0500

    added reverseWords Kata

---
+++ reverseWords/script.js
@@ -0,0 +1,22 @@
+// My Solution
+
+function reverseWords(str) {
+    let newStr = '' // creating string that will eventually be returned
+    const splitWords = str.split(' ') // splitting up multiple words
+    
+    for (let word of splitWords) { // for each word in splitWords...
+      let letters = word.split('') // splitting up the words into letters
+      let newWord = letters.reverse().join('') // reversing then joining back together to a new word
+      newStr = newStr + `${newWord} ` // appending to the new string
+    }
+    
+    return newStr.slice(0, -1) // removing last space that was added
+  }
+
+// Best Solution
+
+function reverseWords(str) {
+    return str.split(' ').map(function(word){
+      return word.split('').reverse().join('');
+    }).join(' ');
+  }
