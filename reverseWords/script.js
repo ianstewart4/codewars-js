201a0da6a29a9e1b81c4976edf0e342ce9ce3710
Ian Stewart <ianstewart4outlook.com>
2022-03-20 15:15:29 -0500

    Added details to reverseWords

--- reverseWords/script.js
+++ reverseWords/script.js
@@ -1,22 +1,31 @@
+/* Details
+
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// Examples
+// "This is an example!" ==> "sihT si na !elpmaxe"
+// "double  spaces"      ==> "elbuod  secaps"
+*/
+
 // My Solution
 
 function reverseWords(str) {
     let newStr = '' // creating string that will eventually be returned
     const splitWords = str.split(' ') // splitting up multiple words
-    
+
     for (let word of splitWords) { // for each word in splitWords...
-      let letters = word.split('') // splitting up the words into letters
-      let newWord = letters.reverse().join('') // reversing then joining back together to a new word
-      newStr = newStr + `${newWord} ` // appending to the new string
+        let letters = word.split('') // splitting up the words into letters
+        let newWord = letters.reverse().join('') // reversing then joining back together to a new word
+        newStr = newStr + `${newWord} ` // appending to the new string
     }
-    
+
     return newStr.slice(0, -1) // removing last space that was added
-  }
+}
 
 // Best Solution
 
 function reverseWords(str) {
-    return str.split(' ').map(function(word){
-      return word.split('').reverse().join('');
+    return str.split(' ').map(function (word) {
+        return word.split('').reverse().join('');
     }).join(' ');
-  }
+}
