e97a8b6731fbe8bbcfbeea1bd88768851460e60b
Ian Stewart <ianstewart4outlook.com>
2022-03-23 19:56:54 -0500

    adding roundup5

---
+++ roundup5/script.js
@@ -0,0 +1,14 @@
+// My solution
+// this one was pretty simple but took a little more time to work out in my mind.
+
+function roundToNext5(n) {
+    return Math.ceil(n / 5) * 5;
+}
+
+// Best solution
+// I think this one is really simple but clever and a little more naturally intuitive
+
+function roundToNext5(n) {
+    while (n % 5 !== 0) n++;
+    return n;
+}
