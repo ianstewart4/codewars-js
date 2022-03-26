fe42833700e1b44ab3639d843d63ca7f6c7b0371
Ian Stewart <ianstewart4@outlook.com>
2022-03-25 23:16:02 -0500

    a word

--- friends/script.js
+++ friends/script.js
@@ -1,5 +1,5 @@
 // My solution was the same as the best solution.
-// This one was extremely simple but practice for me
+// This one was extremely simple but good practice for me
 
 function friend(friends){
     return friends.filter(x => x.length == 4)
