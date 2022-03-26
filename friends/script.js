bf805188ebea223a292619bd871227c535226324
Ian Stewart <ianstewart4@outlook.com>
2022-03-25 23:15:38 -0500

    adding friends

---
+++ friends/script.js
@@ -0,0 +1,7 @@
+// My solution was the same as the best solution.
+// This one was extremely simple but practice for me
+
+function friend(friends){
+    return friends.filter(x => x.length == 4)
+  }
+
