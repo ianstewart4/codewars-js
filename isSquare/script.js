fd2de9643c6edb26e2cabcc8f44f5fa56a3676a8
Ian Stewart <ianstewart4@outlook.com>
2022-03-31 21:58:27 -0500

    adding isSquare

---
+++ isSquare/script.js
@@ -0,0 +1,7 @@
+// Keeping it simple because I'm sicker than I was yesterday, but want to keep the streak alive
+
+var isSquare = function (n) {
+    return Math.sqrt(n) % 1 === 0
+}
+
+// Mine was the same as the best! 
