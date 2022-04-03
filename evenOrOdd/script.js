2cdf1142e7f342cd371554686d6989e02ec3510c
Ian Stewart <ianstewart4@outlook.com>
2022-04-02 21:37:56 -0500

    adding evenOrOdd

---
+++ evenOrOdd/script.js
@@ -0,0 +1,8 @@
+// My solution. Still recovering so keeping it easy with a 7kyu
+// Instructions: Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+function even_or_odd(number) {
+    return number % 2 ? 'Odd' : 'Even' // == 0 which is falsy 
+}
+
+// same as best solution :)
