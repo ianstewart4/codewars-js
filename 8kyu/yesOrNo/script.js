c2599a99338e9f587e35a9418c583ceeb687d75a
Ian Stewart <ianstewart4@outlook.com>
2022-04-16 20:53:09 -0500

    adding yesOrNo

---
+++ 8kyu/yesOrNo/script.js
@@ -0,0 +1,9 @@
+
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+// My solution, same as best. 
+// Very straightforward. If true, return 'yes', else return 'no'
+
+function boolToWord(bool) {
+    return bool ? "Yes" : "No"
+}
