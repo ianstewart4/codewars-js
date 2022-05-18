eed68b9ae75d4a3277770d2addd4b4398e73be23
Ian Stewart <ianstewart4@outlook.com>
2022-05-17 19:19:33 -0500

    adding schoolWork

---
+++ 8kyu/schoolWork/script.js
@@ -0,0 +1,21 @@
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+// Example:
+// n= 5, m=5: 25
+// n=-5, m=5:  0
+// Waiting for translations and Feedback! Thanks!
+
+// My solution
+// I did this one a bit quick and used chained ternaries instead of && 
+
+function paperwork(n, m) {
+    return n >= 0 ? m >= 0 ? n * m : 0 : 0
+}
+
+// Best 
+
+function paperwork(n, m) {
+    return n > 0 && m > 0 ? n * m : 0
+}
