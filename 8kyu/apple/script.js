40e2a1c31c82e3dedd0ed66ca988e9a8e4fa6a1d
Ian Stewart <ianstewart4@outlook.com>
2022-08-26 14:14:53 -0500

    adding apple

---
+++ 8kyu/apple/script.js
@@ -0,0 +1,14 @@
+// Task
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+// My solution
+
+function apple(x) {
+    return x ** 2 > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
+}
+
+// Best
+
+function apple(x) {
+    return Math.pow(x, 2) > 1000 ? 'It\'s hotter than the sun!!' : 'Help yourself to a honeycomb Yorkie for the glovebox.';
+}
