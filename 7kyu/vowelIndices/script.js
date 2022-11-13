448dfb11d15c18447d26fbcab69a1eb59695e67e
Ian Stewart <ianstewart4@outlook.com>
2022-11-13 14:10:03 -0600

    adding vowelIndices

---
+++ 7kyu/vowelIndices/script.js
@@ -0,0 +1,26 @@
+// We want to know the index of the vowels in a given word, for example, there are two vowels in the word super (the second and fourth letters).
+
+// So given a string "super", we should return a list of [2, 4].
+
+// Some examples:
+// Mmmm  => []
+// Super => [2,4]
+// Apple => [1,5]
+// YoMama -> [1,2,4,6]
+// NOTES
+// Vowels in this context refers to: a e i o u y (including upper case)
+// This is indexed from [1..n] (not zero indexed!)
+
+// My solution
+
+function vowelIndices(word) {
+    let vowelPos = []
+    const wordArr = word.split('')
+    for (let i = 0; i < wordArr.length; i++) {
+        if (wordArr[i].toLowerCase().match(/[aeiyou]/)) {
+            vowelPos.push(i + 1)
+        }
+    }
+
+    return vowelPos
+}
