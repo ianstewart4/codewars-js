124921ea76573e4adf3496bf2d08ca28573a34a1
Ian Stewart <ianstewart4@outlook.com>
2022-11-28 15:36:11 -0600

    adding dup

---
+++ 6kyu/dup/script.js
@@ -0,0 +1,15 @@
+// In this Kata, you will be given an array of strings and your task is to remove all consecutive duplicate letters from each string in the array.
+
+// For example:
+
+// dup(["abracadabra","allottee","assessee"]) = ["abracadabra","alote","asese"].
+
+// dup(["kelless","keenness"]) = ["keles","kenes"].
+
+// Strings will be lowercase only, no spaces. See test cases for more examples.
+
+// My solution
+
+function dup(s) {
+    return s.map(w => w.split('').filter((x, i, a) => x !== a[i - 1]).join(''))
+};
